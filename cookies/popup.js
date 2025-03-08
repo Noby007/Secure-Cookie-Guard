5e15a7307bb72eb93fd42c4e6e14d5fc0a6f46f8
NOBY DAYAN <dayan3133@gmail.com>
2025-03-08 10:06:45 +0530

    added import export function

--- cookies/popup.js
+++ cookies/popup.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     const encryptBtn = document.querySelector(".btn-primary:nth-child(1)");
     const decryptBtn = document.querySelector(".btn-primary:nth-child(2)");
+    const importBtn = document.querySelector(".btn-secondary:nth-child(1)");
+    const exportBtn = document.querySelector(".btn-secondary:nth-child(2)");
     const cookieList = document.getElementById("cookieList");
 
     // Function to load cookies
@@ -40,6 +42,39 @@ document.addEventListener("DOMContentLoaded", function () {
 
     decryptBtn.addEventListener("click", function () {
         fakeProcess(decryptBtn, "Decrypt Now");
+    });
+
+    // Import Functionality
+    importBtn.addEventListener("click", function () {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = '.json';
+        input.onchange = e => {
+            const file = e.target.files[0];
+            const reader = new FileReader();
+            reader.onload = event => {
+                const cookies = JSON.parse(event.target.result);
+                cookies.forEach(cookie => {
+                    chrome.cookies.set(cookie);
+                });
+                loadCookies();
+            };
+            reader.readAsText(file);
+        };
+        input.click();
+    });
+
+    // Export Functionality
+    exportBtn.addEventListener("click", function () {
+        chrome.cookies.getAll({}, function (cookies) {
+            const blob = new Blob([JSON.stringify(cookies, null, 2)], { type: 'application/json' });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = 'cookies.json';
+            a.click();
+            URL.revokeObjectURL(url);
+        });
     });
 
     // Load cookies when popup opens
